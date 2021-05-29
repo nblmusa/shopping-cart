3f47a8c81dd3a68f31c6d2155cd0e61501b7fba8
nabilmusa <n.musa@sellanycar.com>
2021-05-30 01:30:46 +0400

    Implement authentication using JWT and role based authorization

---
+++ src/modules/auth/auth.module.ts
@@ -0,0 +1,33 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { UsersModule } from '../users/users.module';
+import { ConfigModule, ConfigService } from '../config';
+import { JwtStrategy } from './jwt.strategy';
+
+@Module({
+  imports: [
+    UsersModule,
+    ConfigModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          secret: configService.get('JWT_SECRET_KEY'),
+          signOptions: {
+            ...(configService.get('JWT_EXPIRATION_TIME')
+              ? {
+                  expiresIn: Number(configService.get('JWT_EXPIRATION_TIME')),
+                }
+              : {}),
+          },
+        };
+      },
+      inject: [ConfigService],
+    }),
+  ],
+  providers: [JwtStrategy],
+  exports: [PassportModule.register({ defaultStrategy: 'jwt' })],
+})
+export class AuthModule {}
